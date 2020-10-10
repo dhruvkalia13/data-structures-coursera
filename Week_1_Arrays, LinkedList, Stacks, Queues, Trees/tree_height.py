c6481b4a735e4facf87f851f82348ad54d3c0d67
Dhruv Kalia <dhruvkalia13@gmail.com>
2020-10-10 18:43:11 +0100

    Week 1 tree_height

---
+++ Week_1_Arrays, LinkedList, Stacks, Queues, Trees/tree_height.py
@@ -0,0 +1,44 @@
+# python3
+
+import sys
+import threading
+
+
+class TreeStructure:
+    def __init__(self):
+        self.n = 0
+        self.parent = []
+        self.cache = []
+
+    def read(self):
+        self.n = int(sys.stdin.readline())
+        self.parent = list(map(int, sys.stdin.readline().split()))
+        self.cache = [0] * self.n
+
+    def path_len(self, node_id):
+        parent = self.parent[node_id]
+        if parent == -1:
+            return 1
+
+        if self.cache[node_id]:
+            return self.cache[node_id]
+
+        self.cache[node_id] = 1 + self.path_len(self.parent[node_id])
+        return self.cache[node_id]
+
+    def compute_height(self):
+        return max([self.path_len(i) for i in range(self.n)])
+
+
+def main():
+    tree = TreeStructure()
+    tree.read()
+    print(tree.compute_height())
+
+
+# In Python, the default limit on recursion depth is rather low,
+# so raise it here for this problem. Note that to take advantage
+# of bigger stack, we have to launch the computation in a new thread.
+sys.setrecursionlimit(10 ** 7)  # max depth of recursion
+threading.stack_size(2 ** 27)  # new thread will get stack of such size
+threading.Thread(target=main).start()
